308e3469f33a62a53475a919b58659c5c48e7ce1
Theau Trova <theau.trova@etu.univ-lyon1.fr>
2019-08-13 09:30:46 +0200

    typo

--- vagrant/script_bashrc.sh
+++ vagrant/script_bashrc.sh
@@ -2,6 +2,6 @@
 echo -e "#additions to bashrc
 
 alias uniflash=\"/opt/ti/uniflash_5.0.0/node-webkit/nw\"
-alias firefoxCopper=\"~/firefox/firefox --no-remote -P firefox55\"
+alias firefoxCopper=\"~/firefox/firefox -no-remote -P firefox55\"
 
 " >> .bashrc
