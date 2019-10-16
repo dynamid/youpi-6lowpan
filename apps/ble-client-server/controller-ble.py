e0ddf827949847daac5a81d2550980edce75f8a7
Jonathan <jonathan@algosecure.fr>
2019-10-16 14:20:43 +0200

    Add a better error exception and logging module

--- apps/ble-client-server/controller-ble.py
+++ apps/ble-client-server/controller-ble.py
@@ -1,5 +1,17 @@
-"""
-TODO: make a docopt interface
+"""controller-ble
+
+A ble controller that handle two ble connection at the same time and 
+forward the data sent from one to the other.  
+
+Usage: 
+    controller-ble --serverHost <ble-mac> [--port <port>] --clientHost <ble-mac> [--debug]
+
+Options: 
+    -h, --help                  Show this help menu.
+    -d, --debug                 Active the debug mode [default: False]
+    -H, --serverHost <ble-mac>  The BLE MAC address of the server.
+    -p, --port <port>           The port use in the ble connection. [default: 3]
+    -c, --clientHost <ble-mac>  The BLE MAC address of the server.
 
 This script is inspired by the uart_collector script of this 
 projet https://github.com/alcir/microbit-ble
@@ -7,6 +19,8 @@ projet https://github.com/alcir/microbit-ble
 import time
 from bluepy import btle
 import bluetooth
+from docopt import docopt, DocoptExit
+import logging
 
 # server6lbr = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 # server6lbr.connect(('B8:27:EB:36:1B:9D', 3))
@@ -18,10 +32,12 @@ def connectRPiServer(address, port):
     try:
         conn.connect((address, port))
     except bluetooth.BluetoothError as be:
-        print("An unexpected error occured: " +  be)
+        logging.error("An unexpected error occured: " +  be)
         conn = None
         
     return conn
+
+
 
 # Handle the notification received by the client from a
 # ble device. 
@@ -36,7 +52,7 @@ class MyDelegate(btle.DefaultDelegate):
         dataToMicrobit = self.server6lbr.recv(1024)
 
         if dataToMicrobit:
-            print("Data received: " + str(dataToMicrobit))
+            logging.debug("Data received: " + str(dataToMicrobit))
             self.serverMicroBit.write(dataToMicrobit)
         
 
@@ -53,10 +69,10 @@ class controllerBLE():
         self._re_connect()
 
     def _connect(self):
-        print("Connecting...")
+        logging.info("Connecting...")
         self.conn = btle.Peripheral(self.device_mac, btle.ADDR_TYPE_RANDOM)
         #self.conn.setSecurityLevel("medium")
-        print("Connected...")
+        logging.info("Connected...")
         self._enable()
 
     def _enable(self):
@@ -71,7 +87,7 @@ class controllerBLE():
 
         self.conn.setDelegate(MyDelegate(self.chwrite, self.server6lbr))
         time.sleep(1)
-        print("UART notification enabled...")
+        logging.info("UART notification enabled...")
 
     def run(self):
         # while True:
@@ -83,7 +99,7 @@ class controllerBLE():
                 # Idle state where no requests are sent by the microbit 
                 
             except Exception as e:
-                print('An unexpected error occured: ' + str(e))
+                logging.error('An unexpected error occured: ' + str(e))
                 self.conn.disconnect()
                 break
 
@@ -96,17 +112,28 @@ class controllerBLE():
                 self._connect()
                 break
             except Exception as e:
-                print(str(e))
+                logging.error(str(e))
                 time.sleep(self._retry_interval_sec)
 
 
 def main():
-    server6lbr = connectRPiServer(address='B8:27:EB:36:1B:9D', port=3)
+    args = docopt(__doc__)
+    serverHost = args['--serverHost']
+    port = int(args['--port']) 
+    clientHost = args['--clientHost']
+    verbose = args['--debug']
+    
+    server6lbr = connectRPiServer(address=serverHost, port=port)
+
+    logging.basicConfig(
+        level=logging.DEBUG if verbose else logging.INFO,
+        format="%(levelname)s:%(message)s"
+    )
     
     if server6lbr is None:
         exit(1)
     
-    mbc = controllerBLE(device_name="microbit", device_mac="E0:14:9E:14:11:72", server=server6lbr, sampling_interval_sec=1)
+    mbc = controllerBLE(device_name="microbit", device_mac=clientHost, server=server6lbr, sampling_interval_sec=1)
     mbc.run()
     
     while True:
@@ -114,4 +141,9 @@ def main():
         pass
     
 if __name__ == '__main__':
-    main()
+    try:
+        docopt(__doc__)
+    except DocoptExit:
+        print(__doc__)
+    else:
+        main()
--- apps/ble-client-server/server.py
+++ apps/ble-client-server/server.py
@@ -1,17 +1,33 @@
-#A simple Python script to receive messages from a client over
-#Bluetooth using PyBluez (with Python 2).
+"""Server
+
+A ble server that gets commands from ble device and forward requests in a 6lowpan 
+network to finally send back the response  
+
+Usage: 
+    server.py --host <ble-mac> [--port <port>] [--debug]
+
+Options: 
+    -h, --help            Show this help menu.
+    -d, --debug           Active the debug mode [default: False]
+    -H, --host <ble-mac>  The BLE MAC address of the server.
+    -p, --port <port>     The port use in the ble connection. [default: 3]
+
+"""
 
 import bluetooth
 from aiocoap import *
 import asyncio
+from docopt import docopt, DocoptExit
+import logging
 
-hostMACAddress = 'B8:27:EB:36:1B:9D'
-port = 3
-backlog = 1
-size = 1024
-addr='[fd00::212:4b00:1204:cb03]' 
-CoAPport='5683'
-f='sen/bar/temp'
+# hostMACAddress = 'B8:27:EB:8C:B2:4F'
+# port = 3
+
+# backlog = 1
+# size = 1024
+# addr='[fd00::212:4b00:1204:cb03]' 
+# CoAPport='5683'
+# f='sen/bar/temp
 
 async def getCoAP(uri):
     protocol = await Context.create_client_context()
@@ -26,30 +42,62 @@ async def post(uri, payload):
     response = await protocol.request(request).response
 
 def main():
-    print("Start the server")
+    args = docopt(__doc__)
+    logging.info("Start the server")
+    hostMACAddress = args["--host"]
+    port = int(args["--port"])
+
+    logging.basicConfig(
+        level=logging.DEBUG if verbose else logging.INFO,
+        format="%(levelname)s:%(message)s"
+    )
+
+    backlog = 1
+    size = 1024
+    addr='[fd00::212:4b00:1204:cb03]' 
+    CoAPport='5683'
+    f='sen/bar/temp'
+    
     s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     s.bind((hostMACAddress, port))
     s.listen(backlog)
-    print("Waiting a client connection")
-    try:
-        client, clientInfo = s.accept()
-        print("Client connected")
-        while 1:
-            data = client.recv(size)
-            if data:
-                data = data.decode("utf-8")
-                print(f'Received data from the controller: {data}')
-                if 'get' in data:
-                    print("CoAP get command sent")
-                    uri = 'coap://'+addr+':'+CoAPport+'/'+f
-                    raw_res = asyncio.get_event_loop().run_until_complete(getCoAP(uri)).decode("utf-8")
-                    res = str(raw_res)
-                    client.send(f"{res},") # Echo back to client
-                    print(f'Data send back to the controller: {res}')
-    except:	
-        print("Closing socket")
-        client.close()
-        s.close()
+    client = None
+    logging.info("Waiting a client connection")
+    while True:
+        try:
+            client, clientInfo = s.accept()
+            logging.info("Client connected")
+            while 1:
+                data = client.recv(size)
+                if data:
+                    data = data.decode("utf-8")
+                    logging.debug('Received data from the controller: ' + data)
+                    if 'get' in data:
+                        logging.debug("CoAP get command sent")
+                        uri = 'coap://'+addr+':'+CoAPport+'/'+f
+                        raw_res = asyncio.get_event_loop().run_until_complete(getCoAP(uri)).decode("utf-8")
+                        res = str(raw_res) + ','
+                        client.send(res) # Echo back to client
+                        logging.debug('Data send back to the controller: ' + res)
 
+        except KeyboardInterrupt:
+            logging.info("Killing process in progress")
+            if client is not None:
+                client.send(b'Deconnection')
+                client.close()
+            s.close()
+            break
+        else:	
+            logging.error("Client deconnected\nWaiting for another client")
+            client.close()
+
+        
+            
 if __name__ == '__main__':
+    try:
+        docopt(__doc__)
+    except DocoptExit:
+        print(__doc__)
+    else:
+        main()
     main()
